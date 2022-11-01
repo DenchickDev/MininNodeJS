90e75e738cf9c01154273773c2cc2f5b060bd59c
Иванов Денис <ltybc3528@mail.ru>
2022-11-01 18:02:17 +0300

    first commit

---
+++ refs/events_ref.js
@@ -0,0 +1,15 @@
+const EventEmiter = require('events');
+
+class Logger extends EventEmiter {
+    log(message) {
+        this.emit('message', `${message} ${Date.now()}`)
+    }
+}
+
+const logger = new Logger();
+
+logger.on('message', data => {
+    console.log(data);
+})
+
+logger.log('Hello')

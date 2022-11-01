efb82a978120d1ec76076d81255a62b79f899c25
Denis Ivanov <ltybc3528@mail.ru>
2022-11-02 01:02:00 +0300

    02 Основы NodeJS. 014 Создание простого Web-сервера

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text">
+                <button type="submit">Send</button>
+            </form>
+        `)
+    }  else if (req.method === 'POST') {
+        const body = [];
+console.log('1');
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8',
 
-    res.write('<h1>Hello form NodeJS</h1>');
-    res.write('<h2>Hello form NodeJS</h2>');
-    res.write('<h3>Hello form NodeJS</h3>');
-    res.end(`
-        <div style="background: red; width: 200px; height: 200px;">
-            <h1>Test 1</h1>
-        </div>
-    `);
+        })
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+            console.log('2');
+        });
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1]
+            console.log('3');
+        res.end(`
+            <h1>Ваше сообщение: ${message}</h1>
+        `);
+
+        })
+
+    }
 });
 
 server.listen(3000, () => {

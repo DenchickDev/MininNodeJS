f8d461e3557f316513c16b3a265b89380aae4871
Иванов Денис <ltybc3528@mail.ru>
2022-11-04 12:22:20 +0300

    03 Практика_ Express.js. 021 Настройка Layout

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars';)
-const app = express();
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+const app = express()
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extmain: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.get('/', (req, res) => {
-    res.render('index');
-});
+  res.render('index')
+})
 
 app.get('/about', (req, res) => {
-    res.render('about');
-});
+  res.render('about')
+})
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })

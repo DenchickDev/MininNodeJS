1cc951354500fedd2a430a304caf85a1c803986b
Denis Ivanov <ltybc3528@mail.ru>
2022-11-27 15:27:12 +0300

    04 Практика_ База данных MongoDB. 043 Добавление товара в корзину

--- models/user.js
+++ models/user.js
@@ -25,4 +25,26 @@ const userSchema = new Schema({
     }
 });
 
+userSchema.methods.addToCart = function(course) {
+    const clonedItems = [...this.cart.items];
+    const idx = clonedItems.findIndex(c => {
+        return c.courseId.toString() === course._id.toString();
+    });
+
+    if (idx >= 0) {
+        clonedItems[idx].count = clonedItems[idx].count + 1;
+    } else {
+        clonedItems.push({
+            courseId: course._id,
+            count: 1
+        })
+    }
+
+    // const newCart = {items: clonedItems};
+
+    this.cart = {items: clonedItems};
+
+    return this.save();
+}
+
 module.exports = model('User', userSchema);

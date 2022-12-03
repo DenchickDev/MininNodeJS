4d850e9a79e6562606557a47a43b3b1fa7c3c4c2
Denis Ivanov <ltybc3528@mail.ru>
2022-12-03 13:01:59 +0300

    05 Практика_ сессии и авторизация. 053 Сессия в базе данных

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
@@ -12,6 +13,7 @@ const authRoutes = require('./routes/auth')
 const User = require('./models/user');
 const varMiddleware = require('./middleware/variables')
 
+const MONGODB_URI = `mongodb://denis:2PFIFbNUlrlKlk9N@ac-z7bws4h-shard-00-00.wjjg1bb.mongodb.net:27017,ac-z7bws4h-shard-00-01.wjjg1bb.mongodb.net:27017,ac-z7bws4h-shard-00-02.wjjg1bb.mongodb.net:27017/?ssl=true&replicaSet=atlas-mf2uuk-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const app = express()
 
 const hbs = exphbs.create({
@@ -20,6 +22,11 @@ const hbs = exphbs.create({
   runtimeOptions: {
     allowProtoPropertiesByDefault: true
   }
+})
+
+const store = MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -31,7 +38,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(session({
   secret: 'some secret value',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }));
 app.use(varMiddleware)
 
@@ -48,19 +56,7 @@ async function start() {
   try {
     const password = '2PFIFbNUlrlKlk9N';
     // const url = `mongodb+srv://denis:2PFIFbNUlrlKlk9N@cluster1.t6p3sge.mongodb.net/?retryWrites=true&w=majority`;
-    const url = `mongodb://denis:2PFIFbNUlrlKlk9N@ac-z7bws4h-shard-00-00.wjjg1bb.mongodb.net:27017,ac-z7bws4h-shard-00-01.wjjg1bb.mongodb.net:27017,ac-z7bws4h-shard-00-02.wjjg1bb.mongodb.net:27017/?ssl=true&replicaSet=atlas-mf2uuk-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    await mongoose.connect(url, {useNewUrlParser: true})
-    
-    // const candidate = await User.findOne();
-    // if (!candidate) {
-    //   const user = new User({
-    //     email: 'ltybc@mail.ru',
-    //     name: 'Denis',
-    //     cart: {items: []}
-    //   });
-
-    //   await user.save();
-    // }
+    await mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
     
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)

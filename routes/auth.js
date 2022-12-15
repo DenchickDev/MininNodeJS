255c378fa1feff33a853eda910e6fc91ce27cd08
Иванов Денис <ltybc3528@mail.ru>
2022-12-15 16:35:53 +0300

    05 Практика_ сессии и авторизация. 060 Сообщения об ошибке

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,9 @@ const router = Router()
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'Авторизация',
-        isLogin: true
+        isLogin: true,
+        loginError: req.flash('loginError'),
+        registerError: req.flash('registerError')
     })
 })
 
@@ -35,8 +37,10 @@ router.post('/login', async (req, res) => {
                 })
             } else {
                 res.redirect('/auth/login#login');
+                req.flash('loginError', 'Неверный пароль');
             }
         } else {
+            req.flash('loginError', 'Такого пользовател не существует');
             res.redirect('/auth/login#login');
         }
     } catch (e) {
@@ -51,6 +55,7 @@ router.post('/register', async (req, res) => {
         const candidate = await User.findOne({email});
 
         if (candidate) {
+            req.flash('reqisterError', 'Пользавтель с таким email существует');
             res.redirect('/auth/login#register')
         } else {
             const hashPassword = await bcrypt.hash(password, 10);

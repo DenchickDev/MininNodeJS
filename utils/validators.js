d28f5506737087c1ce8dcae74513367df63e5e2a
Denis Ivanov <ltybc3528@mail.ru>
2023-01-10 09:35:10 +0300

    07 Практика_ валидация и файлы. 073 Улучшение данных

--- utils/validators.js
+++ utils/validators.js
@@ -1,8 +1,10 @@
 const {body} = require('express-validator/check');
-const User = requrie('../models/user')
+const User = require('../models/user')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Введите корректный email').custom(async (value, {req}) => {
+    body('email')
+    .isEmail().withMessage('Введите корректный email')
+    .custom(async (value, {req}) => {
         try {
             const user = await User.findOne({ email: value });
             if (user) {
@@ -11,14 +13,20 @@ exports.registerValidators = [
         } catch (e) {
             console.log(e);
         }
-    }),
-    body('password', 'Пароль должен быть минимум 6 символов').isLength({min: 6, max: 56}).isAlphanumeric(),
-    body('confirm').custom((value, {req}) => {
+    }).normalizeEmail(),
+    body('password', 'Пароль должен быть минимум 6 символов')
+    .isLength({min: 6, max: 56})
+    .isAlphanumeric()
+    .trim(),
+    body('confirm')
+    .custom((value, {req}) => {
         if (value !== req.body.password) {
             throw new Error('Пароли должны совпадать')
         }
 
         return true;
-    }),
+    })
+    .trim(),
     body('name').isLength({min: 3}).withMessage('Имя должно быть минимум 3 символа')
+    .trim()
 ]

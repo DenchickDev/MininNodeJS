95a9de61cb8d3d8f4b05f0d0a1daefae52b1d08c
Denis Ivanov <ltybc3528@mail.ru>
2023-01-12 16:30:21 +0300

    08 Практика_ деплой приложения. 079 Защита ключей

---
+++ keys/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports =  {
+    MONGODB_URI: `mongodb://denis:2PFIFbNUlrlKlk9N@ac-z7bws4h-shard-00-00.wjjg1bb.mongodb.net:27017,ac-z7bws4h-shard-00-01.wjjg1bb.mongodb.net:27017,ac-z7bws4h-shard-00-02.wjjg1bb.mongodb.net:27017/?ssl=true&replicaSet=atlas-mf2uuk-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    SESSION_SECRET: 'some secret value'
+}
